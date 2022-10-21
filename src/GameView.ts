9eaf392fdc805c6125d6cb197d478769e2d71fba
denis <denis.baklykov@boxberry.ru>
2022-10-21 11:07:46 +0300

    добавляем таблицу (пока не работает)

--- src/GameView.ts
+++ src/GameView.ts
@@ -20,8 +20,23 @@ export class GameView implements IGameView {
     this.el.innerHTML="<div class='gameField'></div><div class='gameControls'></div>"
   }
   updateGameField(field: Cell[][]){
-    let gameField=this.el.querySelector(".gameField");
-
+    let gameField=this.el.querySelector(".gameField") as HTMLDivElement;
+    let tableEl=document.createElement("table");
+    gameField.appendChild(tableEl);
+    for (let row = 0; row < field.length; row++) {
+      let tr=document.createElement("tr");
+      tableEl.appendChild(tr)
+      for (let col = 0; col < field[row].length; col++) {
+         let td=document.createElement("td");
+         if(field[row][col]===1){
+           td.className="cell cell--alive";
+         }
+         else {
+           td.className="cell cell--dead";
+         }
+         tr.appendChild(td);
+      }
+    }
   }
   updateGameState(state: {
     width?: number;
