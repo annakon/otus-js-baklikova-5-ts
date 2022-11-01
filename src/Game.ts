7838ee26da86c8394a71396af000ddc6ab63e2ca
denis <denis.baklykov@boxberry.ru>
2022-11-01 17:31:57 +0300

    все написано, но не работает

--- src/Game.ts
+++ src/Game.ts
@@ -5,5 +5,19 @@ import { Cell } from "./types/Cell";
 export interface IGame {}
 
 export class Game implements IGame {
-    constructor(gameField:IGameField, gameView:IGameView, stepDurationMs?:number){}
+    constructor(gameField:IGameField, gameView:IGameView, stepDurationMs?:number){
+        let timerId: NodeJS.Timer;
+        gameView.onCellClick(gameField.toggleCellState);
+        gameView.onGameStateChange((newState: boolean)=>{
+            if(newState) {
+                timerId = setInterval(()=>{
+                    gameField.nextGeneration();
+                    gameView.updateGameField(gameField.getState());
+                }, stepDurationMs);
+            } else {
+                clearInterval(timerId);
+            }
+        });
+        gameView.onFieldSizeChange(gameField.setSize);
+    }
 }
